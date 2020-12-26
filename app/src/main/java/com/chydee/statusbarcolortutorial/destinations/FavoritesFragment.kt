5e58f14db27997e70912a857f791746965645d4e
Ngwuta Desmond <desmondmuqsit@yahoo.com>
2020-12-26 15:06:37 +0100

    update: added necessary destinations and designed their respective interfaces

---
+++ app/src/main/java/com/chydee/statusbarcolortutorial/destinations/FavoritesFragment.kt
@@ -0,0 +1,25 @@
+package com.chydee.statusbarcolortutorial.destinations
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.chydee.statusbarcolortutorial.R
+
+/**
+ * @author: Desmond Ngwuta
+ * @email: desmondchidi311@gmail.com
+ * @created: 26/12/2020
+ */
+class FavoritesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+
+}
